271dc45e5baee6fcbb78187bbd933350f64fdbfc
Natysoz <Natysoz@gmail.com>
2021-07-01 20:00:11 +0300

    initial state

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyle from "./Constants/GlobalStyle";
+import TopNavigator from "./Components/TopNavigator/TopNavigator";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle/>
+        <TopNavigator/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/TopNavigator/TopNavigator.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const TopNavigator = () => {
+    return (
+        <Container>
+            <div>TopNavigator</div>
+        </Container>
+    )
+}
+
+const Container = styled.div`
+  display: flex;
+`
+
+export default TopNavigator
